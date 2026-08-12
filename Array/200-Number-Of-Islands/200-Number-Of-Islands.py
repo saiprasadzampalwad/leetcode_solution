13f2f50c9dff0aafaea29f71a8d7877bf11d2ec6
saiprasad zampalwad <saiprasadzampalwadjnec@gmail.com>
2026-08-12 22:22:07 +0530

    Number of Islands — python3 (Runtime: 240 ms, Memory: 21.7 MB)

---
+++ Array/200-Number-Of-Islands/200-Number-Of-Islands.py
@@ -0,0 +1,27 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+                return 0
+        rows, cols = len(grid), len(grid[0])
+        count = 0
+
+        def dfs(r, c):
+            if r < 0 or c < 0 or r >= rows or c >= cols:
+                return
+            if grid[r][c] == "0":
+                return
+
+            grid[r][c] = "0"
+
+            dfs(r + 1, c)  # down
+            dfs(r - 1, c)  # up
+            dfs(r, c + 1)  # right
+            dfs(r, c - 1)  # left
+
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == "1":
+                    count += 1
+                    dfs(i, j)
+
+        return count
