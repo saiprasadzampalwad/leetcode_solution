04840d139dd3981820426674aec60d25fb8ea57b
saiprasad zampalwad <saiprasadzampalwadjnec@gmail.com>
2026-08-12 22:23:27 +0530

    Majority Element — java (Runtime: 1 ms, Memory: 56 MB)

---
+++ Array/169-Majority-Element/169-Majority-Element.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+       int candidate=0;
+        int vote=0;
+        for (int i=0;i<nums.length;i++)
+        {
+            if(vote==0)
+            {
+                candidate=nums[i];
+            }
+            if(candidate==nums[i])
+            {
+                vote++;
+
+            }
+            else{
+                vote--;
+            }
+        }
+        return candidate; 
+    }
+}
