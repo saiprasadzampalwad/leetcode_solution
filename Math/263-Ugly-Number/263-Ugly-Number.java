da8d01796244cac651de186f0df5a36cd0a0c5db
saiprasad zampalwad <saiprasadzampalwadjnec@gmail.com>
2026-08-12 22:23:09 +0530

    Ugly Number — java (Runtime: 1 ms, Memory: 42.9 MB)

--- Math/263-Ugly-Number/263-Ugly-Number.java
+++ Math/263-Ugly-Number/263-Ugly-Number.java
@@ -1,12 +1,11 @@
 class Solution {
     public boolean isUgly(int n) {
-        if(n<=0)
+       if(n<=0)
         return false;
-       
-     for(int i :new int[]{2,3,5})
+       for(int i :new int[]{2,3,5})
        { while (n%i==0)
             n=n/i;
-        }
-     return n==1 ;
+       }
+       return n==1 ;
     }
 }
