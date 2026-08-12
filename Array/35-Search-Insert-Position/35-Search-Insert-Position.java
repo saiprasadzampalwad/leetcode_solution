50beb24e25388403001f268aa9c0e74a1450d64b
saiprasad zampalwad <saiprasadzampalwadjnec@gmail.com>
2026-08-12 22:23:36 +0530

    Search Insert Position — java (Runtime: 0 ms, Memory: 45 MB)

---
+++ Array/35-Search-Insert-Position/35-Search-Insert-Position.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int s=0,e=nums.length-1;
+        int m;
+        while(s<=e)
+        {
+            m=s+(e-s)/2;
+            if(nums[m]==target)
+            {
+                return m;
+            }
+            else if(nums[m]>target)
+            {
+                e=m-1;
+            }
+            else
+            {
+                s=m+1;
+            }
+
+        }
+        return s;
+    }
+}
