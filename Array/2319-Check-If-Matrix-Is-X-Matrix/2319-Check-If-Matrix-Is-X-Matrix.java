53b0638fab07ec870d29e118ba93dd0f78b538f8
saiprasad zampalwad <saiprasadzampalwadjnec@gmail.com>
2026-08-12 22:22:46 +0530

    Check if Matrix Is X-Matrix — java (Runtime: 2 ms, Memory: 46.7 MB)

---
+++ Array/2319-Check-If-Matrix-Is-X-Matrix/2319-Check-If-Matrix-Is-X-Matrix.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean checkXMatrix(int[][] grid) {
+        int n = grid.length;
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            for (j = 0; j < n; j++) {
+                if (i == j || i + j == n - 1) {
+                    if (grid[i][j] == 0) {
+                        return false;
+                    }
+                } else {
+                    if (grid[i][j] != 0) {
+                        return false;
+                    }
+
+                }
+            }
+        }
+        return true;
+    }
+}
